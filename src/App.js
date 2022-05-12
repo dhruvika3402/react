6d0a0f35ec783bbae87e900dfec9e4266e98ca89
dhruvika3402 <kananidhruvika@gmail.com>
2022-05-12 09:49:28 +0530

    time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Time from './containers/Time/Time';
+import Timefun from './containers/Time/Timefun';
 // import Country from './containers/country/country';
 // import City from './containers/country/city';
 // import Countryfunc from './containers/country/Countryfunc';
 // import Cityfunc from './containers/country/Cityfunc';
 // import Countryprops from './containers/country/countryprops';
-import Branch from './containers/Branchandcourse/Branch';
+// import Branch from './containers/Branchandcourse/Branch';
+
 
 
 
@@ -19,7 +22,9 @@ function App() {
       <Countryfunc />
       <Cityfunc/> 
       <Countryprops/>     */}
-       <Branch/>
+       {/* <Branch/> */}
+       {/* <Time/> */}
+       <Timefun/>
     </>
   );
 }
